70a902ab88ea410e6aaf1514d5f08208958fc81e
Willyams Yujra <yracnet@gmail.com>
2023-01-04 18:04:17 -0400

    + Fix module build

--- plugin-api/tsup.config.ts
+++ plugin-api/tsup.config.ts
@@ -13,22 +13,8 @@ export default defineConfig([
     sourcemap: false,
   },
   {
-    entry: ["src/plugin/runtime/server.js"],
-    format: "cjs",
-    external: [
-      "virtual:vite-plugin-api:handler",
-      "virtual:vite-plugin-api:router",
-      "virtual:vite-plugin-api:config",
-      "express",
-      "dotenv",
-    ],
-    outDir: "dist/runtime",
-    clean: true,
-    sourcemap: false,
-  },
-  {
-    entry: ["src/plugin/runtime/handler.js"],
-    format: "cjs",
+    entry: ["src/plugin/runtime/server.js", "src/plugin/runtime/handler.js"],
+    format: "esm",
     external: [
       "virtual:vite-plugin-api:handler",
       "virtual:vite-plugin-api:router",
