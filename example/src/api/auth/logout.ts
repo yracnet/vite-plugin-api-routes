17d60ff82aac77cac71180e6dff34bbe771dc939
Willyams Yujra <yracnet@gmail.com>
2023-09-29 09:20:08 -0400

    + Remove all cookies

--- example/src/api/auth/logout.ts
+++ example/src/api/auth/logout.ts
@@ -3,7 +3,9 @@ import { authMiddleware } from "../../common/authMiddleware.js";
 import { createResponseAsync } from "../../common/response.js";
 
 export const POST = async (req: Request, res: Response, next: NextFunction) => {
-  res.clearCookie("auth");
+  ["auth", "article", "user"].forEach(name => {
+    res.clearCookie(name);
+  });
   return createResponseAsync("Logged out!", req, res, next);
 };
 
